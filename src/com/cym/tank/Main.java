a2606cd16f861021cafb0b4ea5fb36866a0fd90e
cym2050 <1390062269@qq.com>
2019-12-16 19:04:31 +0800

    添加键盘监听

--- src/com/cym/tank/Main.java
+++ src/com/cym/tank/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		TankFrame tf = new TankFrame();
-		tf.setVisible(true);
+		
 	}
 
 	
--- src/com/cym/tank/TankFrame.java
+++ src/com/cym/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.cym.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -14,24 +16,40 @@ public class TankFrame extends Frame {
 		setSize(800,600);
 		setResizable(false);
 		setTitle("tank war");
+		setVisible(true);
 		
 		addWindowListener(new WindowAdapter() {
 
 			@Override
 			public void windowClosing(WindowEvent e) {
-				// TODO Auto-generated method stub
 				System.exit(0);
 			}
 				
 		});
+		
+		this.addKeyListener(new MyKeylistener());
 	}
 	@Override 
 	public void paint(Graphics g) {		//图像重新绘制时系统自动调用
 		System.out.println("paint");
-		g.fillRect(x, y, 20, 20);
-		x += 10;
-		y += 10;
+		g.fillRect(x, y, 50, 50);
+		x += 100;
+		y += 100;
 	}
+	
+	class MyKeylistener extends KeyAdapter{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("key pressed");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("key released");
+		}
+	}
+	
 	
 	
 }
