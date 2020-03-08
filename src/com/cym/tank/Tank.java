8e54d13461aa9e997c9c433004025b8e2291bc87
cym2050 <caoyunmei2050@gmail.com>
2020-03-08 14:12:30 +0800

    添加边界检测

--- src/com/cym/tank/Tank.java
+++ src/com/cym/tank/Tank.java
@@ -106,8 +106,22 @@ public class Tank {
 		if(this.group == Group.BAD && random.nextInt(10) > 8) {		//随机方向
 			randomDir();
 		}
+		
+		boundCheck();
 	}
 	
+	private void boundCheck() {
+		// TODO Auto-generated method stub
+		if(this.x < 0) 
+			x = 0;
+		if(this.y < 30)
+			y = 30;
+		if(this.x > TankFrame.GAME_WIDTH - Tank.WIDTH)
+			x = TankFrame.GAME_WIDTH - Tank.WIDTH;
+		if(this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT)
+			y = TankFrame.GAME_HEIGHT - Tank.HEIGHT;
+	}
+
 	private void randomDir() {
 		// TODO Auto-generated method stub
 		this.dir = Dir.values()[random.nextInt(4)];
