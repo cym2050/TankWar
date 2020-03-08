f576186ea3b763ef19624969051484070d18efcd
cym2050 <caoyunmei2050@gmail.com>
2020-03-08 17:09:36 +0800

    了解配置文件

---
+++ src/com/cym/tank/PropertyMgr.java
@@ -0,0 +1,26 @@
+package com.cym.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyMgr {
+	static Properties props = new Properties();
+	
+	static {
+		try {
+			props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object get(String key){
+		if(props == null) 
+			return null;
+		return props.get(key);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(PropertyMgr.get("Initial"));
+	}
+}
