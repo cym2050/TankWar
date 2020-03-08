49db662832ff7aa16a72e6fbdf592bc6d2438315
cym2050 <caoyunmei2050@gmail.com>
2020-03-08 13:37:39 +0800

    调整爆炸的位置

--- src/com/cym/tank/Bullet.java
+++ src/com/cym/tank/Bullet.java
@@ -71,7 +71,9 @@ public class Bullet {
 		if(rect1.intersects(rect2)) {
 			tank.die();
 			this.die();
-			tf.explodes.add(new Explode(this.x, this.y, this.tf));
+			int eX = tank.getX() + tank.WIDTH/2 - Explode.WIDTH/2;
+			int eY = tank.getY() + tank.HEIGHT/2 - Explode.HEIGHT/2;
+			tf.explodes.add(new Explode(eX, eY, this.tf));
 		}
 	}
 
